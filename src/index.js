295ac8bfc7316a054364a8d1ccb1975605c0fa8d
soniya prasad <soniyagupta82270@gmail.com>
2024-02-06 15:10:51 +0530

    update utils

--- src/index.js
+++ src/index.js
@@ -6,24 +6,14 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
-// ;(
-//   //using IIFEs
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server running at ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed");
+  });
 
-//   async () => {
-//     try {
-//       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-//       app.on("error", (error) => {
-//         console.log("Error", error);
-//         throw error;
-//       });
 
-//       app.listen(process.env.PORT, () => {
-//         console.log(`App listening on ${process.env.PORT}`);
-//       });
-//     } catch (error) {
-//       console.log("error is : ", error);
-//       throw error;
-//     }
-//   }
-// )();
---
+++ src/utils/ApiError.js
@@ -0,0 +1,22 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went Wrong",
+    error = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = this.errors;
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.contructor);
+    }
+  }
+}
+
+export { ApiError };
