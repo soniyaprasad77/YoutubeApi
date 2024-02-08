792a975df2cbcec152524f12c986460839bb98fd
soniya prasad <soniyagupta82270@gmail.com>
2024-02-08 13:51:35 +0530

    update user.model.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,49 @@
 import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema({});
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //cloudinary image URL
+      required: true,
+    },
+    cover: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    refreshTokens: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
 export const User = mongoose.model("User", videoSchema);
