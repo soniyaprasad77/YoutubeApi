7564c09b2371ddcb3d6c5e48919b55f89f59218a
soniya prasad <soniyagupta82270@gmail.com>
2024-02-08 14:24:04 +0530

    update video.model.js

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,7 +1,41 @@
 import mongoose, {Schema} from "mongoose";
 
 const videoSchema = new Schema({
-
+ videoFile:{
+  type:String,  //cloudinary URL
+  required:true,
+ },
+ thumbnail:{
+ type:String,  //coludinary URL
+ required:true,
+ },
+ title:{
+ type:String,
+ required:true,
+ },
+ description:{
+    type:String,
+    
+ },
+ duration:{
+    type:Number,
+    required:true,
+ },
+ views:{
+    type:Number,
+    required:true,
+    default:0,
+ },
+ isPublished:{
+    type:Boolean,
+    default:true,
+ },
+ owner:{
+    type:Schema.Types.ObjectId,
+    ref:"User",
+ }
+},{
+    timestamps:true,
 })
 
 export const Video = mongoose.model("Video", videoSchema)
