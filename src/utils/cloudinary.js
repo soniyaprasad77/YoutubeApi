9607715c7186a46e43c208490a895c24f0615e90
soniya prasad <soniyagupta82270@gmail.com>
2024-02-09 21:37:35 +0530

    bug fix : files upload

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,6 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
-import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_API_NAME,
@@ -11,12 +10,21 @@ cloudinary.config({
 const uploadOnCloudinary = async (localFilePath) => {
   try {
     if (!localFilePath) return null;
-    //upload file on cloudinary
-    cloudinary.uploader.upload(localFilePath, { resource_type: "auto" });
-    console.log("file is uploaded on cloudinary", response.url);
+
+    // Upload file on Cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    console.log("File is uploaded on Cloudinary", response.url);
     return response;
   } catch (error) {
+    // Handle error
+    console.error("Error uploading file to Cloudinary:", error.message);
+    // remove the locally saved temporary files as the upload operation got failed
     fs.unlinkSync(localFilePath);
     return null;
   }
 };
+
+export default uploadOnCloudinary;
