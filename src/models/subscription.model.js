0e2db44e7e9c2dc9e60e2a58e8854f02205fb0b6
soniya prasad <soniyagupta82270@gmail.com>
2024-02-21 16:40:43 +0530

    update subscriber model

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,5 +1,17 @@
 import mongoose, { Schema } from "mongoose";
 
-const subscriptionSchema = new Schema({});
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId,
+      ref: "User", // one who is subscribing
+    },
+    channel: {
+      type: Schema.Types.ObjectId, // one to whom subscriber is subscribing
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema);
